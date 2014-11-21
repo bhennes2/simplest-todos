ee693bb196ac328a06f48f4866dc4b9fab877456
bhennes2 <bhennes2@gmail.com>
2014-11-21 13:49:39 -0600

    Small style tweaks and clean up

--- client/templates/lists/list_show.js
+++ client/templates/lists/list_show.js
@@ -41,10 +41,10 @@ Template.listShow.helpers({
 
 Template.task.events({
   "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
     Tasks.update(this._id, {$set: {checked: ! this.checked}});
   },
-  "click .delete": function () {
+  "click .delete": function (event) {
+    event.stopPropagation();
     Tasks.remove(this._id);
   }
 });
